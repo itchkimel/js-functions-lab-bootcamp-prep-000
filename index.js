8d14209ff9a468e229b23b54db2a6a9261ccce92
scrappy-cyborg-4243 <itchkimel@gmail.com>
2020-07-14 18:15:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyCustomHolidayTo (holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 
-function happyCustomHolidayTo (holiday, date){
+function holidayCountdown (holiday, date){
   return `It's ${days} days until ${holiday}!`
 }
